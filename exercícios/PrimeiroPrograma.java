c076a496afb0df0bd9983a734687440367cbdc05
Peterson Carvalho <54187301+PBCarvalho@users.noreply.github.com>
2021-02-04 17:20:06 -0300

    Update PrimeiroPrograma.java

--- exercícios/PrimeiroPrograma.java
+++ exercícios/PrimeiroPrograma.java
@@ -3,6 +3,7 @@ package fundamentos;
 public class PrimeiroPrograma {
 	
 	public static void main(String[] args) {
+		
 		//Organizado por sentença de código terminado com ;
 		System.out.println("Primeiro Programa Parte #01!");
 		System.out.println("Primeiro Programa Parte #02!");
