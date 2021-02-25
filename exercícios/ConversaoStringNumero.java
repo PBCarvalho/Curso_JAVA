d1ab5cf5d0db1a8fec6393610792058a754c62bc
Peterson Carvalho <54187301+PBCarvalho@users.noreply.github.com>
2021-02-24 22:46:26 -0300

    Update ConversaoStringNumero.java

--- exercícios/ConversaoStringNumero.java
+++ exercícios/ConversaoStringNumero.java
@@ -6,8 +6,8 @@ public class ConversaoStringNumero {
 	
 	public static void main(String[] args) {
 		
-		String valor1 = JOptionPane.showInputDialog("Digite o primeiro número:");
-		String valor2 = JOptionPane.showInputDialog("Digite o segundo número:");
+		String valor1 = JOptionPane.showInputDialog("Digite o primeiro número: ");
+		String valor2 = JOptionPane.showInputDialog("Digite o segundo número: ");
 		
 		System.out.println(valor1 + valor2);
 		
