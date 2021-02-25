d29d96ce2e950b4ebec4e5f200cd5ae71243f639
Peterson Carvalho <54187301+PBCarvalho@users.noreply.github.com>
2021-02-24 22:46:19 -0300

    Update ConversaoTipoPrimitivo.java

--- exercícios/ConversaoTipoPrimitivo.java
+++ exercícios/ConversaoTipoPrimitivo.java
@@ -12,19 +12,15 @@ public class ConversaoTipoPrimitivo {
 		float b = (float) 1.1234567899999; // Explícita (CAST)
 		System.out.println(b);
 		
-		int c = 340; /*
-					 * Máximo valor recebido pelo byte 227
-					 * Caso este valor for maior do que o (byte) suporte,
-					 * automaticamente o Java realiza conversão
-					 */
+		int c = 5; // Máximo valor recebido pelo byte 227
+					 // Caso este valor for maior do que o (byte) suporte,
+					 // automaticamente o Java realiza conversão
 		byte d = (byte) c; // Explícita (CAST)
 		System.out.println(d);
 		
 		double e = 1.9999999999;
 		int f = (int) e; // Explícita (CAST)
 		System.out.println(f);
-		
-				
 	}
 
 }
