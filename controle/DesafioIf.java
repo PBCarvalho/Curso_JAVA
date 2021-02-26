d95c8795316fcba939f3f1b4f70f2790a4f9cdb2
Peterson Carvalho <54187301+PBCarvalho@users.noreply.github.com>
2021-02-26 01:43:31 -0300

    Update DesafioIf.java

--- controle/DesafioIf.java
+++ controle/DesafioIf.java
@@ -1,5 +1,17 @@
 package controle;
 
 public class DesafioIf {
+	
+	public static void main(String[] args) {
+		
+		double nota = 1.3;
+		
+		// Neste desafio foi solicitado que fosse identificado o erro
+		
+		if (nota >= 9.0); { // Erro encontrado, não se utiliza (;) após o controle. Mas tem excesão.
+			System.out.println("Quadro de honra!");
+			System.out.println("Você é fera!!!");
+		}
+	}
 
 }
