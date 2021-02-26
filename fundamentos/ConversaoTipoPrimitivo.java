e3dba2ae24e77241dae587a74cbf78f1347dcc42
Peterson Carvalho <54187301+PBCarvalho@users.noreply.github.com>
2021-02-26 01:27:58 -0300

    Create ConversaoTipoPrimitivo.java

---
+++ fundamentos/ConversaoTipoPrimitivo.java
@@ -0,0 +1,26 @@
+package fundamentos;
+
+public class ConversaoTipoPrimitivo {
+	
+	public static void main(String[] args) {
+		
+		//Lembrar que o JAVA não analisa o valor, mas sim o tipo
+		
+		double a = 1; // Implícita
+		System.out.println(a);
+		
+		float b = (float) 1.1234567899999; // Explícita (CAST)
+		System.out.println(b);
+		
+		int c = 5; // Máximo valor recebido pelo byte 227
+					 // Caso este valor for maior do que o (byte) suporte,
+					 // automaticamente o Java realiza conversão
+		byte d = (byte) c; // Explícita (CAST)
+		System.out.println(d);
+		
+		double e = 1.9999999999;
+		int f = (int) e; // Explícita (CAST)
+		System.out.println(f);
+	}
+
+}
