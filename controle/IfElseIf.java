1f77a7b2096e51d85a4d10718ec17fe1a027674d
Peterson Carvalho <54187301+PBCarvalho@users.noreply.github.com>
2021-03-02 10:38:40 -0300

    Update IfElseIf.java

--- controle/IfElseIf.java
+++ controle/IfElseIf.java
@@ -6,7 +6,7 @@ public class IfElseIf {
 	
 	public static void main(String[] args) {
 		
-		Scanner entrada = new Scanner (System.in);
+		Scanner entrada = new Scanner(System.in);
 		
 		System.out.print("Digite a nota: ");
 		
