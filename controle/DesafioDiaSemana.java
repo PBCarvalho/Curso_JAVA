2b3ef7b3b5acd7cfb12abb34546c0fba85050afe
Peterson Carvalho <54187301+PBCarvalho@users.noreply.github.com>
2021-03-02 10:39:06 -0300

    Update DesafioDiaSemana.java

--- controle/DesafioDiaSemana.java
+++ controle/DesafioDiaSemana.java
@@ -6,7 +6,7 @@ public class DesafioDiaSemana {
 	
 	public static void main(String[] args) {
 		
-		Scanner entrada = new Scanner (System.in);
+		Scanner entrada = new Scanner(System.in);
 		
 		System.out.print("Informe um dia da semana: ");
 
