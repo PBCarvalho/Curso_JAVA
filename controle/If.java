e42d036aa8d685951bb25350da77ff0464dd929c
Peterson Carvalho <54187301+PBCarvalho@users.noreply.github.com>
2021-03-02 10:38:49 -0300

    Update If.java

--- controle/If.java
+++ controle/If.java
@@ -6,7 +6,7 @@ public class If {
 	
 	public static void main(String[] args) {
 		
-		Scanner entrada = new Scanner (System.in);
+		Scanner entrada = new Scanner(System.in);
 		
 		System.out.print("Informe a média: ");
 		double media = entrada.nextDouble();
