e15a90ff9759e3cb0e2cb308e2354156adbf44db
Peterson Carvalho <54187301+PBCarvalho@users.noreply.github.com>
2021-03-19 23:40:31 -0300

    Update For3.java

--- controle/For3.java
+++ controle/For3.java
@@ -6,10 +6,9 @@ public class For3 {
 		
 		for(int i = 0; i < 10; i++) {
 			for(int j = 0; j < 10; j++) {
-				System.out.printf("[%d %d}", i, j);
+				System.out.printf("[%d %d]", i, j);
 			}
 			System.out.println();
 		}
 	}
-
 }
