0223eb4a5dca59acab8e8354fe20e4cad98c8d42
Peterson Carvalho <54187301+PBCarvalho@users.noreply.github.com>
2021-04-07 23:31:10 -0300

    Update DataTeste.java

--- classe/DataTeste.java
+++ classe/DataTeste.java
@@ -5,22 +5,22 @@ public class DataTeste {
 	public static void main(String[] args) {
 		
 		Data d1 = new Data();
-		d1.dia = 20;
-		d1.mes = 2;
-		d1.ano = 1981;		
+		// d1.dia = 20;
+		// d1.mes = 2;
+		// d1.ano = 1981;		
 		
-		var d2 = new Data();
-		d2.dia = 2;
-		d2.mes = 7;
-		d2.ano = 2015;
+		var d2 = new Data(2, 7, 2015);
+		// d2.dia = 2;
+		// d2.mes = 7;
+		// d2.ano = 2015;
 		
-		var d3 = new Data();
-		d3.dia = 16;
-		d3.mes = 8;
-		d3.ano = 1993;
+		//var d3 = new Data();
+		//d3.dia = 16;
+		//d3.mes = 8;
+		//d3.ano = 1993;
 		
 		System.out.println("O pai nascem em " + d1.obterDataFormatada());		
 		System.out.println("O filho nasceu em " + d2.obterDataFormatada());
-		System.out.println("A esposa nasceu em " + d3.obterDataFormatada());
+		//System.out.println("A esposa nasceu em " + d3.obterDataFormatada());
 	}
 }
