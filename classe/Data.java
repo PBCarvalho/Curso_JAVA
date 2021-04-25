3fa8242c16514d2e5acacd59b59e5a973d21270e
Peterson Carvalho <54187301+PBCarvalho@users.noreply.github.com>
2021-04-24 21:20:18 -0300

    Update Data.java

--- classe/Data.java
+++ classe/Data.java
@@ -9,18 +9,21 @@ public class Data {
 	
 	//Desafio 02 - DataTeste2
 	Data() {
-		dia = 1;
-		mes = 1;
-		ano = 1970;
+		//dia = 1;
+		//mes = 1;
+		//ano = 1970;
+		this(1, 1, 1970);
 	}
 	
-	Data(int diaInicial, int mesInicial, int anoInicial) {
-		dia = diaInicial;
-		mes = mesInicial;
-		ano = anoInicial;
+	//Desafio 03 - DataTeste2
+	Data(int dia, int mes, int ano) {
+		this.dia = dia;
+		this.mes = mes;
+		this.ano = ano;
 	}
 	
 	String obterDataFormatada() {
-		return String.format("%d/%d/%d", dia, mes, ano);
+		final String formato = "%d/%d/%d";
+		return String.format(formato, dia, mes, ano);
 	}
 }
